a20c9e52109ca83822d4adc45a910e8faab05e90
ilhardika <ilhardika67@gmail.com>
2025-10-12 20:57:42 +0700

    install nextjs & lucide react

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { BookOpenCheck } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-red-500">
+        TEST <BookOpenCheck />
+      </h1>
+    </div>
+  );
+}
